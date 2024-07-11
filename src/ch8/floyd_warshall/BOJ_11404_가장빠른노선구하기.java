6c30f2156498cceda4c86166a61fb2a0e8b7b74c
hjinshin <gudwls818@gmail.com>
2024-07-11 19:27:40 +0900

    BOJ_11404_가장빠른노선구하기

--- src/ch8/floyd_warshall/BOJ_11404_가장빠른노선구하기.java
+++ src/ch8/floyd_warshall/BOJ_11404_가장빠른노선구하기.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class BOJ_11404_가장빠른노선구하기 {
     static int MAX = 10000001;
     static int N, M;
-    static int[][] map;
+    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +14,11 @@ public class BOJ_11404_가장빠른노선구하기 {
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
+        dist = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if(i == j) map[i][j] = 0;
-                else       map[i][j] = MAX;
+                if(i == j) dist[i][j] = 0;
+                else       dist[i][j] = MAX;
             }
         }
         for (int i = 0; i < M; i++) {
@@ -26,14 +26,14 @@ public class BOJ_11404_가장빠른노선구하기 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            map[a][b] = Math.min(map[a][b], c);
+            dist[a][b] = Math.min(dist[a][b], c);
         }
 
         Floyd();
         for(int i = 1; i <= N; i++) {
             for(int j = 1; j <= N; j++) {
-                if(map[i][j] == MAX)    bw.write("0 ");
-                else                    bw.write(map[i][j] + " ");
+                if(dist[i][j] == MAX)    bw.write("0 ");
+                else                    bw.write(dist[i][j] + " ");
             }
             bw.write("\n");
         }
@@ -45,7 +45,7 @@ public class BOJ_11404_가장빠른노선구하기 {
         for(int k=1; k<=N; k++) {
             for(int i=1; i<=N; i++) {
                 for(int j=1; j<=N; j++) {
-                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
