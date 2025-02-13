0114d00ee01d264a60932541ea91cf69193bb3b1
hjinshin <gudwls818@gmail.com>
2025-02-13 18:44:30 +0900

    2025.02.13 BOJ_11053_가장_긴_증가하는_부분_수열

---
+++ src/etc/BOJ_11053_가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,40 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11053_가장_긴_증가하는_부분_수열 {
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i]++;
+            for(int j = i + 1; j < N; j++) {
+                if (arr[i] < arr[j]) {
+                    dp[j] = Math.max(dp[i], dp[j]);
+                }
+            }
+        }
+
+        int max = dp[0];
+        for (int i = 1; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
