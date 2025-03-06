7be53334d9a7f6e7a3bfa65c2167a2dcf6ed9f20
hjinshin <gudwls818@gmail.com>
2025-03-06 15:42:07 +0900

    2025.03.06 BOJ_13549_숨바꼭질3

--- src/etc/BOJ_13549_숨바꼭질3.java
+++ src/etc/BOJ_13549_숨바꼭질3.java
@@ -7,9 +7,11 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_13549_숨바꼭질3 {
+
     static int N, K;
     static int M;
     static int[] dp;
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,23 +30,32 @@ public class BOJ_13549_숨바꼭질3 {
 
     static void bfs() {
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        boolean[] visited = new boolean[M * 2 + 1];
         pq.add(new int[]{N, 0});
+        dp[N] = 0;
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
-            if (dp[cur[0]] <= cur[1]) {
-                continue;
-            }
-            dp[cur[0]] = cur[1];
-            if(cur[0] == K) return;
+            int now = cur[0];
+            int time = cur[1];
+            if(visited[now]) continue;
+            visited[now] = true;
 
-            if (cur[0] * 2 <= 2 * M) {
-                pq.add(new int[]{cur[0] * 2, cur[1]});
+            if (now == K) {
+                result = time;
+                return;
             }
-            if (cur[0] + 1 <= 2 * M) {
-                pq.add(new int[]{cur[0] + 1, cur[1] + 1});
+
+            if (now * 2 <= 2 * M && dp[now * 2] > dp[now]) {
+                dp[now * 2] = dp[now];
+                pq.add(new int[]{now * 2, time});
             }
-            if (cur[0] - 1 >= 0) {
-                pq.add(new int[]{cur[0] - 1, cur[1] + 1});
+            if (now + 1 <= 2 * M && dp[now + 1] > dp[now] + 1) {
+                dp[now + 1] = dp[now] + 1;
+                pq.add(new int[]{now + 1, time + 1});
+            }
+            if (now - 1 >= 0 && dp[now - 1] > dp[now] + 1) {
+                dp[now - 1] = dp[now] + 1;
+                pq.add(new int[]{now - 1, time + 1});
             }
         }
     }
